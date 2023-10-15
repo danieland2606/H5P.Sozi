b391f53bd80607eefa5d987a096603b77e8a80f6
Daniel <daniel.arnold@stud.hs-hannover.de>
2023-10-15 21:20:38 +0200

    Korrektur

--- H5P.Sozi/sozi.js
+++ H5P.Sozi/sozi.js
@@ -1,95 +1,4 @@
-/*
-var H5P = H5P || {};
 
-
-H5P.Sozi = (function ($) {
-     * Constructor function.
-
-    function C(options, id) {
-        // Extend defaults with provided options
-        this.options = $.extend(true, {}, {
-            json: null
-        }, options);
-        // Keep provided id.
-        this.id = id;
-    };
-
-    let framenr= 0;
-
-    let svg_path = H5P.getLibraryPath("H5P.Sozi-1.0") + "/projektarbeiten.svg";
-
-
-    window.onload = async function () {
-        const div = document.getElementById("content");
-        let file = await fetch(svg_path);
-        const text = await file.text();
-        div.innerHTML = text;
-    }
-
-    const json_path = H5P.getPath(this.options.json.path, this.id);
-
-    fetch(json_path)
-        .then(res => res.json())
-        .then(data => {
-            data.frames.forEach((frame) => {
-                Object.keys(frame.cameraStates).forEach((layer) => {
-                    if (layer !== "__sozi_auto__") {
-                        const name = layer;
-                        const cx = frame.cameraStates[layer].cx;
-                        const cy = frame.cameraStates[layer].cy;
-                        const opacity = frame.cameraStates[layer].opacity;
-                        const width = frame.cameraStates[layer].width;
-                        const height = frame.cameraStates[layer].height;
-                        console.log(name)
-                        console.log(cx);
-                        console.log(cy);
-                        console.log(opacity);
-                        console.log(width);
-                        console.log(height);
-                    }
-                });
-            });
-
-        })
-    function nextFrame() {
-
-    }
-
-    function prevFrame() {
-
-    }
-
-    /**
-     * Attach function called by H5P framework to insert H5P content into
-     * page
-     *
-     * @param {jQuery} $container
-
-    C.prototype.attach = function ($container) {
-        let div = document.createElement("div");
-        div.id = "content";
-
-        let nextbtn = document.createElement("button");
-        nextbtn.innerHTML= "Next Frame";
-        nextbtn.addEventListener("click", nextFrame() );
-
-        let backbtn = document.createElement("button");
-        backbtn.innerHTML= "Previous Frame";
-        nextbtn.addEventListener("click", prevFrame() );
-
-        let buttondiv = document.createElement("div");
-        buttondiv.append(nextbtn);
-        buttondiv.append(backbtn);
-
-        $container.append(div);
-        $container.append(buttondiv);
-    };
-
-
-
-    return C;
-})(H5P.jQuery);
- */
 var H5P = H5P || {};
 
 H5P.Sozi = (function ($) {
@@ -166,8 +75,8 @@ H5P.Sozi = (function ($) {
         /**
          * Erstellung von einem ButtonDiv, welcher die Buttons enthält.
          */
-        let buttondiv = document.createElement("div");
-        buttondiv.className = "buttonDivClass";
+        let buttonDiv = document.createElement("div");
+        buttonDiv.className = "buttonDivClass";
 
         /**
          * Erstellung von einem Button der zum vorherigen Frame springt.
@@ -192,8 +101,8 @@ H5P.Sozi = (function ($) {
         /**
          * Das Anhängen von den Buttons zu der ButtonDiv.
          */
-        buttondiv.append(backbtn);
-        buttondiv.append(nextbtn);
+        buttonDiv.append(backbtn);
+        buttonDiv.append(nextbtn);
 
         /**
          * Erstellung von einem FrameDiv um die aktuelle Frame Nummer anzuzegien.
@@ -362,7 +271,7 @@ H5P.Sozi = (function ($) {
          * Anhängen der jewweilien Divs an den Container.
          */
         $container.append(div);
-        $container.append(buttondiv);
+        $container.append(buttonDiv);
         $container.append(frameDiv);
     };
 
